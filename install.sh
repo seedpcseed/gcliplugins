7ab56aa5baefd1921b6e2ff8ac445ae1bcba0164
seedpcseed <seedpcseed@gmail.com>
2025-08-29 10:28:16 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-REPO="https://github.com/<owner>/gcliplugins" # Replace <owner> with the repository owner's GitHub username or organization
+REPO="https://github.com/seedpcseed/gcliplugins" # Replace <owner> with the repository owner's GitHub username or organization
 TEMP_DIR="$(mktemp -d)"
 trap 'rm -rf "$TEMP_DIR"' EXIT
 git clone --depth 1 "$REPO" "$TEMP_DIR" >/dev/null 2>&1
